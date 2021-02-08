9914abc8cc68d3ce3c66b5d409dd41675175d90f
José Manuel Barroso Galindo <theypsilon@gmail.com>
2021-02-08 23:35:16 +0100

    Update install_and_check_mras.sh

--- install_and_check_mras.sh
+++ install_and_check_mras.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "Running MiSTer Updater"
+echo
+
 UPDATER_URL=${UPDATER_URL:-"https://raw.githubusercontent.com/MiSTer-devel/Updater_script_MiSTer/master/update.sh"}
 
 curl --fail --location "${UPDATER_URL}" > update.sh
@@ -8,6 +11,9 @@ chmod +x update.sh
 
 ./update.sh
 rm update.sh
+
+echo
+echo "Running MRA Checker"
 
 ./mra_checker.py --ignore-roms --mra-folder "delme/_Arcade"
 
