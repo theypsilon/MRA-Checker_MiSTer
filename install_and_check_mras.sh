1142dd9bd544e2a025ae4723a51e3523505a0fac
José Manuel Barroso Galindo <theypsilon@gmail.com>
2021-12-07 11:37:12 +0100

    Using downloader

--- install_and_check_mras.sh
+++ install_and_check_mras.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Running MiSTer Updater"
+echo "Running MiSTer Downloader"
 echo
 
-UPDATER_URL=${UPDATER_URL:-"https://raw.githubusercontent.com/MiSTer-devel/Updater_script_MiSTer/master/update.sh"}
+DOWNLOADER_URL=${DOWNLOADER_URL:-"https://raw.githubusercontent.com/MiSTer-devel/Downloader_MiSTer/main/dont_download.sh"}
 
-curl --fail --location "${UPDATER_URL}" > update.sh
-chmod +x update.sh
+curl --fail --location "${DOWNLOADER_URL}" > downloader.sh
+chmod +x downloader.sh
 
-./update.sh
-rm update.sh
+./downloader.sh
+rm downloader.sh
 
 echo
 echo "Running MRA Checker"
